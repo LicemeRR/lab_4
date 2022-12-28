7046043e521c3087437703f118c54ef92dfe061e
LicemeR <80973735+LicemeRR@users.noreply.github.com>
2022-12-28 21:45:55 +0200

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string words[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "¬вед≥ть слово є" << i + 1 << ": ";
+		cin >> words[i];
+	}
+
+	for (int i = 9; i >= 0; i--)
+	{
+		cout << words[i] << " ";
+	}
+}
